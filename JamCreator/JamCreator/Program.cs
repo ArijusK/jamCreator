f85f1852fa4ebcc1b5bec7e7f7f117e67b7ae254
Arijus Kaminskas <arijus.kaminskas@gmail.com>
2025-10-04 16:34:14 +0300

    Built and ran the project and added /api/hello endpoint to the server

--- JamCreator/JamCreator/Program.cs
+++ JamCreator/JamCreator/Program.cs
@@ -1,13 +1,27 @@
 using JamCreator.Client.Pages;
 using JamCreator.Components;
 
+using System.Net.Http;
+using Microsoft.AspNetCore.Components;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
 
+// Basic HttpClient for components rendered via the server
+builder.Services.AddHttpClient();
+
+// BaseAddress = current app origin (so you can call "api/..." with a relative URL)
+builder.Services.AddScoped(sp =>
+    new HttpClient { BaseAddress = new Uri(sp.GetRequiredService<NavigationManager>().BaseUri) });
+
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,14 +35,19 @@ else
     app.UseHsts();
 }
 
+
+
 app.UseHttpsRedirection();
 
 
 app.UseAntiforgery();
 
+app.MapGet("/api/hello", () => Results.Ok(new { message = "Hello from the server!" }));
+
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(JamCreator.Client._Imports).Assembly);
+
 
 app.Run();
