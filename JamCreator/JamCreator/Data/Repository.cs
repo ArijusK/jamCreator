9ba2b92dd5bae42f52fa86489d6664deda2c2172
Justinas Jarmalavičius <justinas.jarmalvicius@mif.stud.vu.lt>
2025-11-25 13:21:45 +0200

    Added database exception handling middleware

--- JamCreator/JamCreator/Data/Repository.cs
+++ JamCreator/JamCreator/Data/Repository.cs
@@ -1,11 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using JamCreator.Shared.Models;
 using JamCreator.Shared.Interfaces;
-using System.Linq.Expressions;   
+using System.Linq.Expressions;
 
 namespace JamCreator.Data
 {
@@ -21,40 +22,85 @@ namespace JamCreator.Data
         }
 
         public async Task<T?> GetByIdAsync(TKey id, CancellationToken ct = default)
-            => await _db.FirstOrDefaultAsync(e => e.Id!.Equals(id), ct);
+        {
+            try
+            {
+                return await _db.FirstOrDefaultAsync(e => e.Id!.Equals(id), ct);
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseException("Failed to fetch entity by ID.", ex);
+            }
+        }
 
         public async Task<List<T>> ListAsync(Expression<Func<T, bool>>? predicate = null, CancellationToken ct = default)
         {
-            IQueryable<T> q = _db.AsNoTracking();
-            if (predicate is not null) q = q.Where(predicate);
-            return await q.ToListAsync(ct);
+            try
+            {
+                IQueryable<T> q = _db.AsNoTracking();
+                if (predicate is not null) q = q.Where(predicate);
+                return await q.ToListAsync(ct);
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseException("Failed to fetch entity list.", ex);
+            }
         }
 
         public async Task AddAsync(T entity, CancellationToken ct = default)
         {
-            await _db.AddAsync(entity, ct);
-            await _context.SaveChangesAsync(ct);
+            try
+            {
+                await _db.AddAsync(entity, ct);
+                await _context.SaveChangesAsync(ct);
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseException("Failed to add entity.", ex);
+            }
         }
 
         public async Task UpdateAsync(T entity, CancellationToken ct = default)
         {
-            _db.Update(entity);
-            await _context.SaveChangesAsync(ct);
+            try
+            {
+                _db.Update(entity);
+                await _context.SaveChangesAsync(ct);
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseException("Failed to update entity.", ex);
+            }
         }
 
         public async Task DeleteAsync(T entity, CancellationToken ct = default)
         {
-            _db.Remove(entity);
-            await _context.SaveChangesAsync(ct);
+            try
+            {
+                _db.Remove(entity);
+                await _context.SaveChangesAsync(ct);
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseException("Failed to delete entity.", ex);
+            }
         }
 
         public async Task<bool> DeleteByIdAsync(TKey id, CancellationToken ct = default)
         {
-            var e = await GetByIdAsync(id, ct);
-            if (e is null) return false;
-            _db.Remove(e);
-            await _context.SaveChangesAsync(ct);
-            return true;
+            try
+            {
+                var e = await GetByIdAsync(id, ct);
+                if (e is null) return false;
+
+                _db.Remove(e);
+                await _context.SaveChangesAsync(ct);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new DatabaseException("Failed to delete entity by ID.", ex);
+            }
         }
     }
 }
